ed74f1b21f93df26f4b45e5a8522de2a58f60968
Nick M <mully7773@gmail.com>
2022-09-30 10:51:51 -0500

    Fix absolute positioning issue with overflow box element

--- js/accordian.js
+++ js/accordian.js
@@ -1,1 +1,8 @@
 "use-strict";
+
+const accordion = document.querySelector(".question-container");
+accordion.addEventListener("click", function (e) {
+  const accordionItem = e.target.closest(".question");
+  if (!accordion) return;
+  accordionItem.classList.toggle("open");
+});
